e663a1c5b28368181e94e03a62ade54f3206c000
Zafer Balkan <zafer@zaferbalkan.com>
2021-01-30 11:07:20 +0300

    Added global error handlong with Error Banner and Error MessageBox.

--- NetBannerNG/Program.cs
+++ NetBannerNG/Program.cs
@@ -27,10 +27,17 @@ namespace NetBannerNG
 
             Application.ApplicationExit += new EventHandler(Application_ApplicationExit);
             AppDomain.CurrentDomain.ProcessExit += new EventHandler(CurrentDomain_ProcessExit);
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(MyHandler);
 
             Application.Run(banner);
 
             GC.KeepAlive(mutex);                // mutex shouldn't be released - important line
+        }
+
+        private static void MyHandler(object sender, UnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(((Exception)e.ExceptionObject).Message, "An Unhandled Error Occured.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            Application.Run(Banner.Error());
         }
 
         private static void CurrentDomain_ProcessExit(object sender, EventArgs e)
