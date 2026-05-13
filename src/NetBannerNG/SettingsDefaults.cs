0fb8b0bf090e60fc5bf3f4e6bd1e0010721f957b
Zafer Balkan <zafer@zaferbalkan.com>
2026-05-13 17:38:17 +0300

    Added default values hardcoded.

---
+++ src/NetBannerNG/SettingsDefaults.cs
@@ -0,0 +1,17 @@
+namespace NetBannerNG;
+
+internal static class SettingsDefaults
+{
+    internal const int DefaultBannerSize = 28;
+    internal const string DefaultClassificationProfile = "NOT_CONFIGURED";
+    internal const int DefaultCustomSettings = 0;
+    internal const string DefaultCustomBackgroundColor = "#FFFFFF";
+    internal const string DefaultCustomForeColor = "#000000";
+    internal const int DefaultInfoCon = 0;
+    internal const int DefaultFpCon = 0;
+    internal const int DefaultCaveatsEnabled = 0;
+    internal const int DefaultDisableBorders = 0;
+    internal const int DefaultShowHostInformation = 0;
+    internal const int DefaultEnableBottomBanner = 0;
+    internal const string PolicyRegistryPath = @"SOFTWARE\Policies\NetBannerNG";
+}
